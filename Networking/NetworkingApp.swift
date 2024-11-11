977857d17b9b46a0cb2454f3b8faa796e0a8ac7f
Andrii Boichuk <andrii.boichuk@yahoo.com>
2024-11-11 13:19:56 +0100

    Project init.

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Andrii Boichuk on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkingTests/NetworkingTests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingTests.swift
+//  NetworkingTests
+//
+//  Created by Andrii Boichuk on 11/11/2024.
+//
+
+import Testing
+
+struct NetworkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
